93c7fe6dbbae3558efa81317a743893fa290b8a0
Graham Swan <thinkswan@gmail.com>
2012-04-30 15:29:09 -0600

    updated dummy app test pages

--- test/dummy/app/controllers/landing_controller.rb
+++ test/dummy/app/controllers/landing_controller.rb
@@ -1,11 +1,6 @@
 class LandingController < ApplicationController
   def index
-    # Load all app.yml settings (if the initializer exists)
-    if defined? App
-      @app = App
     # If the initializer hasn't been installed yet, display instructions on how to install it using the provided generator
-    else
-      render :missing_initializer
-    end
+    render :missing_initializer unless defined? App
   end
 end
