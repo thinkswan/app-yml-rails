a714a88cac1f1d8b93703bd31ba3b0c077aa14fc
Graham Swan <thinkswan@gmail.com>
2012-05-01 01:41:51 -0600

    fixed deep_update! bug where rails server wouldn't start

--- lib/generators/app_yml/install/templates/app.rb
+++ lib/generators/app_yml/install/templates/app.rb
@@ -1,3 +1,50 @@
+# Public: Extends Ruby's Hash class to provide a method for performing a deep update on a hash.
+class Hash
+  # Public: Performs an in-place deep update on the calling hash with the provided 'new_hash'. Allows us to override 'app.yml'
+  # settings from the 'all' environment with the current environment (test/development/staging/production) with unlimited nesting.
+  #
+  # new_hash - The hash to copy over the calling hash.
+  #
+  # Examples
+  #
+  #   { :email => 'email@example.com', :name => 'Graham Swan' }.deep_update!({ :email => 'new_email@example.com' })
+  #   => { :email => 'new_email@example.com', :name => 'Graham Swan' }
+  #
+  # Returns the calling hash with the updated values.
+  def deep_update! new_hash={}
+    new_hash.each_pair do |key, val|
+      if val.class.eql? Hash
+        self[key] = {} if self[key].nil? # Include settings defined in a specific environment (even when they're not defined in the 'all' environment)
+        self[key].deep_update! val
+      else
+        self[key] = val
+      end
+    end
+    
+    self
+  end
+end
+
+# Public: Extends Rails' HashWithIndifferentAccess class to provide a method for accessing a hash's values with dot syntax.
+module ActiveSupport
+  class HashWithIndifferentAccess < Hash
+    # Public: Causes '{}.arg' to call '{}[:arg]'.
+    #
+    # Note: None of the HashWithIndifferentAccess methods will work here. (http://as.rubyonrails.org/classes/HashWithIndifferentAccess.html)
+    #   The aforementioned methods will invoke their expected functionality.
+    #
+    # Examples
+    #
+    #   (HashWithIndifferentAccess.new({ :country => 'Canada' })).country
+    #   => 'Canada'
+    #
+    # Returns the calling hash with the updated values.
+    def method_missing(method, *args)
+      self[method]
+    end
+  end
+end
+
 # Public: An initializer that provides global access to the App constant (which holds all settings from config/app.yml).
 #
 # Thanks to Adrian Danieli (http://sickpea.com/2009/6/rails-app-configuration-in-10-lines) for this idea.
